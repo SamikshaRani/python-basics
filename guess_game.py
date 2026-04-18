a987167ee5272bac68a820191e8f1099d46913b4
Samiksha Rani <samiksharani249@gmail.com>
2026-04-19 00:10:15 +0530

    Update guessing game

--- guess_game.py
+++ guess_game.py
@@ -1,12 +1,34 @@
 import random
+def play_game():
+    number = random.randint(1, 50)
+    print("You have five attempts to guess the number.")
 
-number=random.randint(1,10)
+    attempts=0
+    while attempts < 5:
+        guess = int(input("Guess number (1-50): "))
 
-while True:
-    guess=int(input("Guess number (1-10): "))
-               
-    if guess==number:
-        print("Correct!")
-        break
+        if guess < 1 or guess > 50:
+            print("Invalid input! Enter between 1 and 50.")
+            continue
+        attempts += 1
+
+        if guess == number:
+            print("Correct! You won in", attempts, "attempts.")
+            break
+        else:
+            if abs(guess - number) <= 5:
+                print("Very close!")
+            elif guess > number:
+                print("Too high!")
+            else:
+                print("Too low!")
+
+    if attempts == 5 and guess != number:
+        print("Game Over! The number was", number)
+
+    choice = input("Do you want to play the guessing game again? (yes/no): ")
+    if choice.lower() == "yes":
+        play_game()
     else:
-        print("Try again")
+        print("Thanks for playing! Goodbye!")
+play_game()
