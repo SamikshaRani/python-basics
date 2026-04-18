f8b91e7131319b8e414a5ad139d892d66389eec6
Samiksha Rani <samiksharani249@gmail.com>
2026-04-18 11:33:04 +0530

    guess_game.py
    A simple number guessing game using Python where the user repeatedly guesses a randomly generated number until the correct answer is found. Demonstrates loops, conditionals, and user inout hamdling.

--- guess_game.py
+++ guess_game.py
@@ -1,9 +1,12 @@
 import random
 
 number=random.randint(1,10)
-guess=int(input("Guess number(1-10): "))
 
-if guess==number:
-    print("Correct!")
-else:
-    print("Wrong! Number was", number)
+while True:
+    guess=int(input("Guess number (1-10): "))
+               
+    if guess==number:
+        print("Correct!")
+        break
+    else:
+        print("Try again")
