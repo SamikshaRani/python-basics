b7b043cd96cf107f916b20fe284f1d4b4bec16cc
Samiksha Rani <samiksharani249@gmail.com>
2026-04-20 10:54:46 +0530

    Updating the guessing game with difficulty levels and making it more user friendly by adding options to play as long as  the user wants.

--- guess_game.py
+++ guess_game.py
@@ -1,11 +1,26 @@
 import random
 def play_game():
+    print("\n---Welcome to the Guessing Game!---")
+    level = input(" Choose difficulty level (easy, medium, hard): ").lower()
+     
+    difficulties = {"easy": 10 , "medium": 5 , "hard": 3}
+    attempts_allowed = difficulties.get(level, 5)
+
+    if level not in difficulties:
+        print("Invalid Input! Defaulting tp medium difficulty.")
+
     number = random.randint(1, 50)
-    print("You have five attempts to guess the number.")
+    print("You have", attempts_allowed , "attempts to guess the number.")
 
     attempts=0
-    while attempts < 5:
-        guess = int(input("Guess number (1-50): "))
+    won=False
+
+    while attempts < attempts_allowed:
+        try:
+            guess = int(input("Guess number (1-50): "))
+        except ValueError:
+            print("Invalid input! Enter a number.")
+            continue
 
         if guess < 1 or guess > 50:
             print("Invalid input! Enter between 1 and 50.")
@@ -14,6 +29,7 @@ def play_game():
 
         if guess == number:
             print("Correct! You won in", attempts, "attempts.")
+            won=True
             break
         else:
             if abs(guess - number) <= 5:
@@ -23,12 +39,18 @@ def play_game():
             else:
                 print("Too low!")
 
-    if attempts == 5 and guess != number:
+    if not won:
         print("Game Over! The number was", number)
 
-    choice = input("Do you want to play the guessing game again? (yes/no): ")
-    if choice.lower() == "yes":
-        play_game()
-    else:
+while True:
+    play_game()
+    while True:
+        choice = input("Do you want to play the guessing game again? (yes/no): ").lower()
+    
+        if choice in [ "yes" , "no"]:
+            break
+        else:
+            print("Please enter only 'yes' or 'no'.")
+    if choice == "no":
         print("Thanks for playing! Goodbye!")
-play_game()
+        break
